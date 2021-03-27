fca0b1031068908cdef127268e109809873112fd
JacksonMarcony <jacklujack23@gmail.com>
2021-03-27 10:58:18 -0300

    feat: send email with mailtjet

--- src/services/send-mail.js
+++ src/services/send-mail.js
@@ -1,1 +1,33 @@
-const mail = require('node-mailjet')
+require('dotenv').config()
+
+const mailjet = require ('node-mailjet')
+
+const request = mailjet.connect(process.env.API_KEY, process.env.SECRET_KEY)
+.post("send", {'version': 'v3.1'})
+.request({
+  "Messages":[
+    {
+      "From": {
+        "Email": process.env.EMAIL_FROM,
+        "Name": "Jackson Marcony"
+      },
+      "To": [
+        {
+          "Email": "teste@gmail.com",
+          "Name": "Jackson Marcony"
+        }
+      ],
+      "Subject": "Greetings from Mailjet.",
+      "TextPart": "My first Mailjet email",
+      "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+      "CustomID": "AppGettingStartedTest"
+    }
+  ]
+})
+request
+  .then((result) => {
+    console.log(result.body)
+  })
+  .catch((err) => {
+    console.log(err.statusCode)
+  })
