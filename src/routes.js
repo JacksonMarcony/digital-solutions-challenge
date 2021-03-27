26d7b794e0b0f249dca702c32a2fca5027bed0d5
JacksonMarcony <jacklujack23@gmail.com>
2021-03-27 11:21:10 -0300

    add: import controller and export route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 
+const LeadController = require('./controllers/leadController') 
+
 routes
     .get('/api/v1/leads', )
-    .post('/api/v1/leads', )
+    .post('/api/v1/leads', LeadController.index)
+
+
+module.exports = routes
