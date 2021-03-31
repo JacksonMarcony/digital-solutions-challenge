0399000f18d96e4d00f5abe390f76c3966fb0ec8
JacksonMarcony <jacklujack23@gmail.com>
2021-03-30 23:02:05 -0300

    refactor: send email more personalizated

--- src/services/send-mail.js
+++ src/services/send-mail.js
@@ -1,33 +1,36 @@
 require('dotenv').config()
-
 const mailjet = require ('node-mailjet')
 
-const request = mailjet.connect(process.env.API_KEY, process.env.SECRET_KEY)
-.post("send", {'version': 'v3.1'})
-.request({
-  "Messages":[
-    {
-      "From": {
-        "Email": process.env.EMAIL_FROM,
-        "Name": "Jackson Marcony"
-      },
-      "To": [
-        {
-          "Email": "teste@gmail.com",
+exports.sendMail = async ( name, email, title, description ) => {
+  const request = mailjet.connect(process.env.API_KEY, process.env.SECRET_KEY)
+  .post("send", {'version': 'v3.1'})
+  .request({
+    "Messages":[
+      {
+        "From": {
+          "Email": process.env.EMAIL_FROM,
           "Name": "Jackson Marcony"
-        }
-      ],
-      "Subject": "Greetings from Mailjet.",
-      "TextPart": "My first Mailjet email",
-      "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
-      "CustomID": "AppGettingStartedTest"
-    }
-  ]
-})
-request
-  .then((result) => {
-    console.log(result.body)
+        },
+        "To": [
+          {
+            "Email": email,
+            "Name": name
+          }
+        ],
+        "Subject": `${title}`,
+        "TextPart": "Hoje é um dia especial",
+        "HTMLPart": `
+        <h2>${description}</h2>
+        `,
+        "CustomID": "AppGettingStartedTest"
+      }
+    ]
   })
-  .catch((err) => {
-    console.log(err.statusCode)
-  })
+  request
+    .then((result) => {
+      console.log("sended email with successful")
+    })
+    .catch((err) => {
+      console.log("There was an error sending the email: ", err)
+    })
+}
