368b4e9b5a55426abd201035df04ed6511cbcde5
JacksonMarcony <jacklujack23@gmail.com>
2021-03-31 01:02:03 -0300

    feat: listen and create leads

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -1,12 +1,43 @@
-require('dotenv').config()
-
-const mongoose = require('../schema/')
+const mongoose = require('../schema').lead
 
 exports.index = async (req, res) => {
-    await new mongoose({
-        name: 'joao',
-        email: 'jose@gmail.com'
-    })
+    try {
+        const leads = await mongoose.find()
+        return res.json(leads)
 
-    return res.json('vlw')
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+}
+
+exports.create = async (req, res) => {
+    const { email } = req.body
+    const { name } = req.verifyDatas
+
+    await new mongoose({
+        name,
+        email
+    })
+    .save()
+    .then(resolve => {
+        const { id, name, email, date } = resolve
+        return res.status(201).json({
+            "response": "Lead created with successful",
+            "status": 201,
+            "lead": {
+                id,
+                name,
+                email,
+                date
+            }
+        })
+    })
+    .catch((error) => {
+        return res.json({
+            error,
+            "status": 500
+        })
+    })
 }
